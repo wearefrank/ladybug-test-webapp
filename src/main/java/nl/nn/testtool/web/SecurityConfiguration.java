8cd026921939839de3eec0d5be69847237d3c18d
Martijn Dirkse <martijn@integrationpartners.nl>
2025-07-11 13:40:41 +0200

    Allow all requests

---
+++ src/main/java/nl/nn/testtool/web/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package nl.nn.testtool.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+}
