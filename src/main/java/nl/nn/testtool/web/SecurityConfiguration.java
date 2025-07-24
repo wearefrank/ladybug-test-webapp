403dd9dcb75d6161c2c0ff523314a95551db541d
Martijn Dirkse <martijn@integrationpartners.nl>
2025-07-24 16:29:51 +0200

    Does not work: trying to add a security configuration

--- src/main/java/nl/nn/testtool/web/SecurityConfiguration.java
+++ src/main/java/nl/nn/testtool/web/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package nl.nn.testtool.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry.requestMatchers("/**").permitAll())
+                .build();
     }
 }
